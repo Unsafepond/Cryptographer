cab07ade0c31605a2593eefbfd65f1375f853636
Jerrod Junker <UnsafePond@Jerrods-MacBook-Pro.local>
2015-05-28 15:24:13 -0600

    Through flexible rotation

--- cryptographer.rb
+++ cryptographer.rb
@@ -1,53 +1,82 @@
+require 'pry'
 
 class Encryptor
-  attr_accessor :message
-  def initialize(message)
-    @message = message
-  end
 
-  
+	def self.encrypt(message, key_size)
 
-end
+	   alphabet_lowercase = ('a'..'z').to_a
+	   	alphabet_lowercase_rotated = alphabet_lowercase.rotate(key_size)
+	    alphabet_lowercase_combined = Hash[alphabet_lowercase.zip(alphabet_lowercase_rotated)]
 
-class Decryptor
-  # Your code here
+	    alphabet_uppercase = ('A'..'Z').to_a
+	    alphabet_uppercase_rotated = alphabet_uppercase.rotate(key_size)
+	    alphabet_uppercase_combined = Hash[alphabet_uppercase.zip(alphabet_uppercase_rotated)]
+
+	    encryption = []
+	    characters = message.split("")
+	    characters.each do |char|
+	      if char == " "
+	        encryption << " "
+	      elsif char == char.upcase
+	        encryption << alphabet_uppercase_combined[char]
+	      else
+	        encryption << alphabet_lowercase_combined[char]
+	      end
+	    end
+	    puts encryption.join
+	    encryption.join
+	end
+
 end
 
 class EncryptionEngine
 
-  def encrypt(message)
+	def encrypt(message, key_size)
+		message.length
+		Encryptor.encrypt(message, key_size)
 
-    encrypt_hash = Hash.new
+	end
 
-   alphabet_lowercase = ('a'..'z').to_a
-   	alphabet_lowercase_rotated = alphabet_lowercase.rotate(13)
-    alphabet_lowercase_combined = Hash[alphabet_lowercase.zip(alphabet_lowercase_rotated)]
-
-    alphabet_uppercase = ('A'..'Z').to_a
-    alphabet_uppercase_rotated = alphabet_uppercase.rotate(13)
-    alphabet_uppercase_combined = Hash[alphabet_uppercase.zip(alphabet_uppercase_rotated)]
-
-
-    encryption = []
-    characters = message.split("")
-    characters.each do |char|
-      if char == " "
-        encryption << " "
-      elsif char == char.upcase
-        encryption << alphabet_uppercase_combined[char]
-      else
-        encryption << alphabet_lowercase_combined[char]
-      end
-    end
-
-    encryption.join
-
-
-  end
+	def decrypt(message, key_size)
+		Decryptor.decrypt(message, key_size)
+	end
 
 end
 
 
+class Decryptor
+
+	def self.decrypt(message, key_size)
+
+	   alphabet_lowercase = ('a'..'z').to_a
+	   	alphabet_lowercase_rotated = alphabet_lowercase.rotate(-key_size)
+	    alphabet_lowercase_combined = Hash[alphabet_lowercase.zip(alphabet_lowercase_rotated)]
+
+	    alphabet_uppercase = ('A'..'Z').to_a
+	    alphabet_uppercase_rotated = alphabet_uppercase.rotate(-key_size)
+	    alphabet_uppercase_combined = Hash[alphabet_uppercase.zip(alphabet_uppercase_rotated)]
+
+	    decryption = []
+	    characters = message.split("")
+	    characters.each do |char|
+	      if char == " "
+	        decryption << " "
+	      elsif char == char.upcase
+	        decryption << alphabet_uppercase_combined[char]
+	      else
+	        decryption << alphabet_lowercase_combined[char]
+	      end
+	    end
+	    puts decryption.join
+	    decryption.join
+	end
+end
+
+
+# engine = EncryptionEngine.new
+# engine.encrypt("My message")
+# EncryptionEngine(Encryptor.new("My Message"))
 engine = EncryptionEngine.new
- encryptor = Encryptor.new("My Message")
-engine.encrypt("My Message")
+engine.encrypt("My Message", 13)
+engine.decrypt("Zl Zrffntr", -13)
+# encryptor = Encryptor.new("My message")
--- cryptographer_test.rb
+++ cryptographer_test.rb
@@ -4,7 +4,13 @@ require_relative 'cryptographer'
 class EncryptionEngineTest < Minitest::Test
   def test_it_encrypts_using_rot13
     engine = EncryptionEngine.new
-    output = engine.encrypt("My Message")
+    output = engine.encrypt("My Message", 13)
     assert_equal "Zl Zrffntr", output
+  end
+
+  def test_it_decrypts_using_rot13
+    engine = EncryptionEngine.new
+    output = engine.decrypt("Zl Zrffntr", 13)
+    assert_equal "My Message", output
   end
 end
